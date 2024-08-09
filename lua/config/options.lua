4d8e7ad8c5e5a5250793f9015c13de78b7b2504f
TheDarkStarJack <2403220952@qq.com>
2024-08-09 20:26:09 +0800

    update config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,21 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- ----------------------------------------------------------------------
+-- 编码设置
+-- ----------------------------------------------------------------------
+
+if vim.fn.has('multi_byte') == 1 then
+    -- 内部工作编码
+    vim.o.encoding = 'utf-8'
+
+    -- 文件默认编码
+    vim.o.fileencoding = 'utf-8'
+
+    -- 打开文件时自动尝试下面顺序的编码
+    vim.o.fileencodings = 'ucs-bom,utf-8,gbk,gb18030,big5,euc-jp,latin1'
+end
+
+-- 设置自动换行
+vim.opt.wrap = true
