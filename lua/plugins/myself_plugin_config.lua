3269f76bd4468dbad5eeb51778d6fb01383d020d
TheDarkStarJack <2403220952@qq.com>
2024-11-12 17:33:10 +0800

    update config

--- lua/plugins/myself_plugin_config.lua
+++ lua/plugins/myself_plugin_config.lua
@@ -45,14 +45,10 @@ return {
   --
   -- vim 的数据库界面
   {
-    "tpope/vim-dadbod",
-  },
-
-  {
     "kristijanhusak/vim-dadbod-ui",
     dependencies = {
       { "tpope/vim-dadbod", lazy = true },
-      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql", "postgresql" }, lazy = true }, -- Optional
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
     },
     cmd = {
       "DBUI",
@@ -64,5 +60,19 @@ return {
       -- Your DBUI configuration
       vim.g.db_ui_use_nerd_fonts = 1
     end,
+  },
+  { -- optional saghen/blink.cmp completion source
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        -- add vim-dadbod-completion to your completion providers
+        completion = {
+          enabled_providers = { "lsp", "path", "snippets", "buffer", "dadbod" },
+        },
+        providers = {
+          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+        },
+      },
+    },
   },
 }
