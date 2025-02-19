6c00375af3fd5ef3236fd004b1b6027c4285f134
TheDarkStarJack <2403220952@qq.com>
2025-02-20 04:40:38 +0800

    fixed : 注释bigfile文件类型

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -122,20 +122,20 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   end,
 })
 
-vim.filetype.add({
-  pattern = {
-    [".*"] = {
-      function(path, buf)
-        return vim.bo[buf]
-            and vim.bo[buf].filetype ~= "bigfile"
-            and path
-            and vim.fn.getfsize(path) > vim.g.bigfile_size
-            and "bigfile"
-          or nil
-      end,
-    },
-  },
-})
+-- vim.filetype.add({
+--   pattern = {
+--     [".*"] = {
+--       function(path, buf)
+--         return vim.bo[buf]
+--             and vim.bo[buf].filetype ~= "bigfile"
+--             and path
+--             and vim.fn.getfsize(path) > vim.g.bigfile_size
+--             and "bigfile"
+--           or nil
+--       end,
+--     },
+--   },
+-- })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
   group = augroup("bigfile"),
