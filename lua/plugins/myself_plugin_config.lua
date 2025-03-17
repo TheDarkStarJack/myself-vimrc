fd3b2eb220355f8829dcb6c4b4d9c3bddebf5da0
TheDarkStarJack <2403220952@qq.com>
2025-03-17 16:09:59 +0800

    add plugin：akinsho/nvim-toggleterm.lua 用于终端支持；JayDoubleu/vim-pwsh-formatter 用于 powershell 格式化

--- lua/plugins/myself_plugin_config.lua
+++ lua/plugins/myself_plugin_config.lua
@@ -135,9 +135,6 @@ return {
   {
     "whitestarrain/md-section-number.nvim",
     ft = { "markdown", "md" },
-    config = function()
-      require("md-section-number").setup()
-    end,
   },
 
   -- 中英文自动空格
@@ -231,4 +228,41 @@ return {
   --     },
   --   },
   -- },
+
+  -- 添加 terminal 终端支持
+  {
+    "akinsho/nvim-toggleterm.lua",
+    config = function()
+      require("toggleterm").setup({
+        size = 20,
+        open_mapping = [[<c-\>]],
+        hide_numbers = true, -- hide the number column in toggleterm buffers
+        shade_filetypes = {},
+        shade_terminals = true,
+        shading_factor = "3", -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+        start_in_insert = true,
+        insert_mappings = true, -- whether or not the open mapping applies in insert mode
+        persist_size = true,
+        direction = "horizontal",
+        close_on_exit = true, -- close the terminal window when the process exits
+        shell = vim.o.shell, -- change the default shell
+        float_opts = {
+          border = "single",
+          width = 200,
+          height = 50,
+          winblend = 3,
+          highlights = {
+            border = "Normal",
+            background = "Normal",
+          },
+        },
+      })
+    end,
+  },
+
+  -- 添加 powershell 格式化支持
+  {
+    "JayDoubleu/vim-pwsh-formatter",
+    ft = { "ps1" },
+  },
 }
